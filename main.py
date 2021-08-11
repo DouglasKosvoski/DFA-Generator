a9048b87c11306baba91562a2cc99103a8075b61
Douglas Kosvoski <douglas.contactpro@gmail.com>
2021-08-11 19:26:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,14 @@
 Author: Douglas Kosvoski
 Email: douglas.contactpro@gmail.com
 
-Construction of an application to construct, determinate and minify
-(eliminate dead and unreachable grammar rules) of finite automata.
-This program executes the token load (reserved words, operators, special symbols, etc...) and 
-A aplicação executa a carga de tokens (palavras reservadas, operadores, símbolos especiais, ...) e Regular Grammars (RG) from a given text file.
+  Construction of an application to construct, determinate and minify
+  (eliminate dead and unreachable grammar rules) of finite automata.
+  This program executes the token load (reserved words, operators, special symbols, etc...) and 
+  Regular Grammars (RG) from a given text file.
 
-Input: file with the token and/or grammar relations from a 
-Entrada: arquivo com a relação de tokens e/ou GRs dos tokens de uma linguagem hypothetical language.
-
-Output: deterministic finite automaton (DFA), free from dead and unreacheable states.
-into a CSV file table representation
+  Input: file with the token and/or grammar relations from a linguagem hypothetical language.
+  
+  Output: deterministic finite automaton (DFA), free from dead and unreacheable states into a CSV file table representation.
 """
 
 def clear_file_data(filename="input.in"):
